ea807ca74ae6d4b179410c2e8e4374c6896157d7
batlord666 <105217288+batlord666@users.noreply.github.com>
2023-02-11 10:57:34 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utg-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">   
+
+<!--title----------------->
+<title>GoMama.World</title>
+
+<!--stylesheet------------>
+<link rel="stylesheet" href="css/style.css"/>
+
+<!--fav-icon-------------->
+<link rel="shortcut icon" href="images/test.png"/>
+
+<!--poppins--------------->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+</head>
+
+ <!--Navbar---------- -->
+ <div class="nav">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href='login.php'>Login</a></li>
+    </ul>
+</div>
+
